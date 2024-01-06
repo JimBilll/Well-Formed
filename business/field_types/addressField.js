dbe059459625ca43e449a12316cf84b1c23c08c7
James McLean <jm2022@hw.ac.uk>
2024-01-06 13:28:28 +0000

    Dates and Addresses
    - Developed generateField() method of DateField
    - Developed generateField() method of AddressField
    - Added examples of these to testFile.js

--- business/field_types/addressField.js
+++ business/field_types/addressField.js
@@ -1,11 +1,27 @@
 export default class AddressField {
-    constructor(fieldName, isRequired){
+    constructor(fieldName, noOfLines, isRequired){
         // TODO - complete constructor method
         this.name = fieldName;
+        this.lines = noOfLines;
         this.required = isRequired;
     }
 
-    generateField(){
+    generateField() {
         // TODO complete generateField method
+        var newField = "";
+        newField += '<label>' + this.name + '</label>';
+
+        for (var i = 1; i <= this.lines; i++) {
+            newField += '\n\t<label for="Address Line ' + i + '">Address Line ' + i + '</label>';
+            
+            if (this.required) {
+                newField +='\n\t<input type="text" id="Address Line ' + i + '" name="Address Line ' + i + '" aria-label="' + this.name + ' Address Line "' + i + ' required>';
+            }
+            else {
+                newField +='\n\t<input type="text" id="Address Line ' + i + '" name="Address Line ' + i + '" aria-label="' + this.name + ' Address Line "' + i + ' >';
+            }
+        }
+
+        return newField;
     }
 }
