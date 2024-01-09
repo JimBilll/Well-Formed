fe73bd4f2ef3c49ed510dbb11a3efa1c59cd7b00
James McLean <jm2022@hw.ac.uk>
2024-01-09 12:16:06 +0000

    Radio Buttons
    - Developed the generateField() method for the RadioField class.
    - May need more accessibility features.
    - Added a radio field to the test form.

--- business/field_types/radioField.js
+++ business/field_types/radioField.js
@@ -1,11 +1,25 @@
 export default class RadioField{
-    constructor(fieldName, isRequired){
+    constructor(fieldName, fieldOptions, isRequired){
         // TODO - complete constructor method
         this.name = fieldName;
+        this.options = fieldOptions;
         this.required = isRequired;
     }
 
     generateField(){
         // TODO complete generateField method
+        var newField = "";
+        newField += '<label>' + this.name + '</label>';
+
+        for (var i = 0; i < this.options.length; i++) {
+            if (this.required) {
+                newField +='\n\t<input type="radio" id="' + this.options[i] + '" name="' + this.name + '" required>';
+            }
+            else {
+                newField +='\n\t<input type="radio" id="' + this.options[i] + '" name="' + this.name + '">';
+            }
+            newField += '\n\t<label for="' + this.options[i] + '">' + this.options[i] + '</label>';
+        }
+        return newField;
     }
 }
--- business/testing/testFile.js
+++ business/testing/testFile.js
@@ -1,17 +1,19 @@
+import * as fs from 'fs';
 import Form from "../form.js";
 import TextField from "../field_types/textField.js";
 import EmailField from "../field_types/emailField.js";
 import DateField from "../field_types/dateField.js";
 import AddressField from "../field_types/addressField.js";
 import DropdownField from "../field_types/dropdownField.js";
-import * as fs from 'fs';
+import RadioField from "../field_types/radioField.js";
 
 var fields = [new TextField("First Name", true),
               new TextField("Last Name", false),
               new EmailField("Email Address", true),
               new DateField("Date of Birth", true),
               new AddressField("Residence", 3, false),
-              new DropdownField("Preferred colour", ["red", "blue", "green"], true)];
+              new DropdownField("Preferred colour", ["red", "blue", "green"], true),
+              new RadioField("Pick One", ["Option 1", "Option 2", "Option 3"], true)];
 
 var testForm = new Form(fields);
 
