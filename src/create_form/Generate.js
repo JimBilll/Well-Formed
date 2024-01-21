497fbe947a297fb6c49f504b3a876aa2ec74bae2
James McLean <jm2022@hw.ac.uk>
2024-01-21 15:02:50 +0000

    Required checkbox and better data management
    - Added “required” checkbox to text field input
    - Data for the form is now held as direct objects in the array rather than as lists of raw data
    - Simplified the code in Generate.js to reflect the new structure of data

--- src/create_form/Generate.js
+++ src/create_form/Generate.js
@@ -2,13 +2,7 @@ import Form from "form-generator/form";
 import textField from "form-generator/field_types/textField";
 
 export default function generate(data) {
-    var formData = []
-
-    data.forEach(element => {
-        formData.push(new textField(element, false));
-    });
-
-    var formElement = new Form(formData);
+    var formElement = new Form(data);
 
     return formElement.generateForm();
 }
--- src/create_form/field_inputs/TextFieldInput.js
+++ src/create_form/field_inputs/TextFieldInput.js
@@ -1,15 +1,22 @@
+import TextField from "form-generator/field_types/textField";
+
 export default function TextFieldInput(id, data, setData) {
     
     return (
         <div>
             <h3>Text Input</h3>
             <label>Field Name: </label>
-            <input type="text" onChange={e => updateData(e)}></input>
+            <input type="text" id={"textInputName" + id} onChange={e => updateData(e)}></input>
+            <label>Required: </label>
+            <input type="checkbox" id={"textInputRequired" + id} onChange={e => updateData(e)}></input>
         </div>
     );
     function updateData(e) {
         var newData = data;
-        data[id] = e.target.value;
+        var name = document.getElementById("textInputName" + id).value;
+        var required = document.getElementById("textInputRequired" + id).checked;
+        
+        data[id] = new TextField(name, required);
         setData(newData);
     }
 }
