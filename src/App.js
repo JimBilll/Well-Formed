f18e70e439d3992e87311c4ff0011842def36681
James McLean <jm2022@hw.ac.uk>
2024-01-23 17:20:06 +0000

    Branding
    - Added a the "Good Form" product name to the app with a logo and the title of the page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import CreateForm from './create_form/CreateForm';
 function App() {
   return (
     <div className="App">
+      <div className="App-nav">
+        <h1>Good Form</h1>
+        <p>ACCESSIBLE WEBFORMS AT YOUR FINGERTIPS</p>
+      </div>
       <header className="App-header">
         <CreateForm />
       </header>
