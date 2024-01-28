7e29c3e03cd60909b80ddb7781906ad9b49ad47c
James McLean <jm2022@hw.ac.uk>
2024-01-28 18:12:21 +0000

    Address Line Slider
    - Added a slider for selecting the number of line for an Address Field
    - Added a new favicon

--- src/create_form/field_inputs/AddressFieldInput.js
+++ src/create_form/field_inputs/AddressFieldInput.js
@@ -8,7 +8,8 @@ export default function AddressFieldInput(id, data, setData) {
             <label>Field Name: </label>
             <input type="text" id={"addressInputName" + id} onChange={e => updateData(e)}></input>
             <label>Number of Address Lines: </label>
-            <input type="text" id={"addressInputLines" + id} onChange={e => updateData(e)}></input>
+            <input type="range" min="3" max="7" defaultValue="3" id={"addressInputLines" + id} onChange={e => updateData(e)}></input>
+            <label id={"addressInputLinesNumber" + id}>3</label>
             <label>Required: </label>
             <input type="checkbox" id={"addressInputRequired" + id} onChange={e => updateData(e)}></input>
             <button type="button" onClick={() => deleteField()}>Delete</button>
@@ -19,7 +20,9 @@ export default function AddressFieldInput(id, data, setData) {
         var newData = data;
         var name = document.getElementById("addressInputName" + id).value;
         var lines = document.getElementById("addressInputLines" + id).value;
+        document.getElementById("addressInputLinesNumber" + id).innerText = lines; // update the slider counter on the UI
         var required = document.getElementById("addressInputRequired" + id).checked;
+        
         
         data[id] = new AddressField(name, lines, required);
         setData(newData);
